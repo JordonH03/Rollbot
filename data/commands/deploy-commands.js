83302f87c40c9b3b32e7a2a4a4c884df98f5a835
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2022-02-28 09:43:28 -0600

    Update package.json
    Updated the version number

--- data/commands/deploy-commands.js
+++ data/commands/deploy-commands.js
@@ -1,11 +1,11 @@
 const fs = require('node:fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('../config.json');
+const { clientId, guildId, token } = require('../../config.json');
 
 const commands = [];
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./data/commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
